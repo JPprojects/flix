317a2f5697d67b37c41c973bee0c3fe068f48d2c
GlenHani <glen.hani@beis.gov.uk>
2020-06-22 15:45:19 +0100

    Added Tests FOR Autho

--- Flix.API/Startup.cs
+++ Flix.API/Startup.cs
@@ -11,7 +11,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
 
 namespace Flix.API
 {
@@ -31,11 +30,6 @@ namespace Flix.API
             services.AddDbContext<FlixContext>(options =>
                  options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
-
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
-            });
 
         }
 
@@ -64,13 +58,6 @@ namespace Flix.API
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
-            });
-
-            app.UseSwagger();
-
-            app.UseSwaggerUI(c =>
-            {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
             });
         }
     }
