37d36298702340fda57280bf75b0a89a3ca7aac6
GlenHani <glen.hani@beis.gov.uk>
2020-06-22 14:57:04 +0100

    Add Db Context for user

---
+++ Flix.API/Models/FlixContext.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+namespace AcebookApi.Models
+{
+    public class PostContext : DbContext
+    {
+        public PostContext(DbContextOptions<PostContext> options) : base(options)
+        {
+        }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        // protected override void OnConfiguring(DbContextOptionsBuilder optionBuilder)
+        // {
+        //     optionBuilder.UseNpgsql("");
+        // }
+    }
+}
