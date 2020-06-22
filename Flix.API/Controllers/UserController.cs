f94334ead6b0dc602b16ee60cfc2dc2b7ba1975a
GlenHani <glen.hani@beis.gov.uk>
2020-06-22 15:38:13 +0100

    Add User Context

--- Flix.API/Controllers/UserController.cs
+++ Flix.API/Controllers/UserController.cs
@@ -2,28 +2,26 @@ using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Linq;
 using AcebookApi.Models;
-using Acebook.Api.Models;
-using System;
-using Microsoft.AspNetCore.Http;
+using Flix.API.Repo;
 
-namespace Flix.API.Controllers
+namespace AcebookApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class UserController : Controller
+    public class PostController : ControllerBase
     {
-        private readonly  _context;
+        private readonly FlixContext _context;
 
-        public UserController(PostContext context)
+        public PostController(FlixContext context)
         {
             _context = context;
-
         }
 
         [HttpGet]
-        public ActionResult<List<User>> GetAll()
+        public ActionResult<IEnumerable<User>> GetAll()
         {
-            return _context.Users.ToList();
+            var userRepo = new UserRepository(_context);
+            return userRepo.ViewAll();
         }
 
         [HttpGet("{id}", Name = "GetUser")]
@@ -37,77 +35,12 @@ namespace Flix.API.Controllers
             return item;
         }
 
-        [HttpPost(Name = "SignUp")]
-        [Route("/User/SignUp")]
-        public ActionResult SignUp()
+        [HttpPost]
+        public object Create(User post)
         {
-            string username = Request.Form["username"];
-            string emailAddress = Request.Form["emailAddress"];
-            string password = Request.Form["password"];
-            string firstName = Request.Form["firstName"];
-            string lastName = Request.Form["lastName"];
-
-            var user = new User()
-            {
-                UserName = username,
-                Password = password,
-                FirstName = firstName,
-                LastName = lastName,
-                EmailAddress = emailAddress
-            };
-
-           var result = _context.Users.FirstOrDefault(c => c.UserName == user.UserName);
-
-            if (result != null)
-            {
-                return RedirectToAction("Sign_Up", "Home");
-            }
-            else
-            {
-                var encyrt = new EncrytpionRepository(user.Password).ReturnEncrpyt();
-                user.Password = encyrt;
-                _context.Users.Add(user);
-                _context.SaveChanges();
-                return RedirectToAction("Log_in", "Home");
-            }
+            _context.Users.Add(post);
+            _context.SaveChanges();
+            return post;
         }
-
-
-
-        [HttpPost(Name = "Login")]
-        [Route("/User/Login")]
-        public IActionResult Login()
-        {
-            string username = Request.Form["username"];
-            string password = Request.Form["password"];
-
-
-            var user = _context.Users.FirstOrDefault(i => i.UserName == username);
-            
-
-
-            if (user != null)
-            {
-                var db_password = user.Password;
-                var doesItMatch = new AuthoRepository();
-                var result = doesItMatch.SignInValidation(db_password, password);
-
-
-                if (result == true)
-                {
-                    HttpContext.Session.SetString("username", user.UserName);
-                    HttpContext.Session.SetString("UserId", user.Id.ToString());
-                    return RedirectToAction("Index", "Post");
-                 } else {
-                    return RedirectToAction("Log_in", "Home");
-                };
-            }
-            else
-            {
-                return RedirectToAction("Log_in", "Home");
-            }
-
-        }
-
     }
 }
--- Flix.API/Repo/UserRepository.cs
+++ Flix.API/Repo/UserRepository.cs
@@ -1,10 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AcebookApi.Models;
+
 namespace Flix.API.Repo
 {
     public class UserRepository
     {
-        public UserRepository()
+        public readonly FlixContext _context;
+
+        public UserRepository(FlixContext context)
         {
+            _context = context;
+        }
+
+
+        public List<User> ViewAll()
+        {
+            return _context.Users.ToList();
         }
     }
 }
