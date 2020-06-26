eaf7965720ad6ff69d072e1bfae28f5c6ca8d460
GlenHani <glen.hani@beis.gov.uk>
2020-06-26 13:52:09 +0100

    results for seterlized

---
+++ Flix.API/Models/Results.cs
@@ -0,0 +1,12 @@
+public class Stat
+{
+    public int Popularity { get; set; }
+    public string price { get; set; }
+    public string profitability_ltc { get; set; }
+    public int algo { get; set; }
+    public string speed { get; set; }
+    public string profitability_btc { get; set; }
+    public string profitability_above_btc { get; set; }
+    public string profitability_eth { get; set; }
+    public string profitability_above_eth { get; set; }
+}
