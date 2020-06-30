49ec5b946c4ad520d1905556b06e38980328ce6a
GlenHani <glen.hani@beis.gov.uk>
2020-06-30 11:31:39 +0100

    Commit changes for test

--- Flix.API/Repo/Playlist/PlaylistRepository.cs
+++ Flix.API/Repo/Playlist/PlaylistRepository.cs
@@ -23,7 +23,7 @@ namespace Flix.API.Repo.Users
             var playlist = new Playlist()
             {
                 Title = title,
-                UserId = 1
+                UserId = UserId
             };
             context.Playlists.Add(playlist);
             context.SaveChanges();
@@ -41,6 +41,7 @@ namespace Flix.API.Repo.Users
         public Playlist EditPlayList(string title, int Id)
         {
             var playlist = GetPlaylistById(Id);
+
             playlist.Title = title;
             context.SaveChanges();
             return playlist;
